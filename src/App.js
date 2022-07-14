efebd847cf578d1b0e60149f0abda08206f17a14
laexy <alex31wk@gmail.com>
2022-07-14 17:34:18 +1000

    react-toastify alerts added for errors in SignIn/Up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Navbar from "./components/Navbar";
-import Explore from "./pages/Explore";
-import Offers from "./pages/Offers";
-import Profile from "./pages/Profile";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import ForgotPassword from "./pages/ForgotPassword";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Navbar from './components/Navbar';
+import Explore from './pages/Explore';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   return (
@@ -21,6 +23,8 @@ function App() {
         </Routes>
         <Navbar />
       </Router>
+
+      <ToastContainer />
     </>
   );
 }
