610ba7b60a61df36cc7bae0eeb28f33319408051
laexy <alex31wk@gmail.com>
2022-07-15 11:30:08 +1000

    Added Google OAuth

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -10,6 +10,7 @@ import {
 } from 'firebase/auth';
 import { setDoc, doc, serverTimestamp } from 'firebase/firestore';
 import { db } from '../firebase.config';
+import OAuth from '../components/OAuth';
 
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
@@ -111,7 +112,8 @@ function SignUp() {
             </button>
           </div>
         </form>
-        {/* Google OAuth */}
+        
+        <OAuth />
 
         <Link to='/sign-in' className='registerLink'>
           Sign In Instead
